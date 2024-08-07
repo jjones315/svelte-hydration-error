cff815b05cfb4f2be34b3cbbaba4ff07cd998d41
Jayce Jones <jayce.jones@trinityonline.com>
2024-08-07 14:37:47 -0500

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import Cell from "./Cell.svelte";
+</script>
+
+<table>
+    <thead>
+        <tr>
+            <Cell>Cell</Cell>
+        </tr>
+    </thead>
+</table>
---
+++ src/routes/Cell.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import type { HTMLAttributes } from "svelte/elements";
+
+    type CellProps = HTMLAttributes<HTMLDivElement> & {
+        as?: "td" | "th";
+    };
+
+    let { as = "td", children }: CellProps = $props();
+</script>
+
+<svelte:element this={as}>
+    {#if children}
+        {@render children()}
+    {/if}
+</svelte:element>
